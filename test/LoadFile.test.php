3c0cee2a59b1cafd41f5644697008a5e311cac0d
Greg Bowler <greg.bowler@g105b.com>
2015-02-07 11:27:53 +0000

    Add first test

---
+++ test/LoadFile.test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Tests the loading of CSV files is done as expected.
+ *
+ * http://github.com/g105b/phpcsv
+ * @copyright Copyright Ⓒ 2015 Bright Flair Ltd. (http://brightflair.com)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace g105b\phpcsv;
+
+class LoadFile_Test extends \PHPUnit_Framework_TestCase {
+
+public function testTest() {
+	$this->assertTrue(true);
+}
+
+}#
