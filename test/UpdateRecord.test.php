099709edb820fa130681f1a17355c84a7a719a72
Greg Bowler <greg.bowler@g105b.com>
2015-02-18 16:48:02 +0000

    Add UpdateRecord test case

---
+++ test/UpdateRecord.test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Tests updating existing rows in a CSV file.
+ *
+ * http://github.com/g105b/phpcsv
+ * @copyright Copyright Ⓒ 2015 Bright Flair Ltd. (http://brightflair.com)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace g105b\phpcsv;
+
+class UpdateRecord_Test extends \PHPUnit_Framework_TestCase {
+}#
