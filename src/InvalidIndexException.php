6d306944879c52ddebf02976a6a01fc7f602b1b2
Greg Bowler <greg.bowler@g105b.com>
2015-03-04 10:52:48 +0000

    Add InvalidIndexException
    closes #11

---
+++ src/InvalidIndexException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Thrown when an index is given that is non-integer or less than zero.
+ *
+ * @copyright Copyright Ⓒ 2015 Bright Flair Ltd. (http://brightflair.com)
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ */
+namespace g105b\phpcsv;
+
+class InvalidIndexException extends \Exception {}#
